60ed96f334b2da3b0d9b5ceaf637453a2c5a3742
wufeiteng <wufeiteng@bytedance.com>
2024-05-21 16:55:19 +0800

    I need some prompt engineering

--- translator.py
+++ translator.py
@@ -2,6 +2,7 @@ from typing import List
 
 from langchain_community.llms import Ollama
 from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.prompts import PromptTemplate
 from langchain.chains import create_retrieval_chain
 # from langchain_openai import ChatOpenAI
 from langchain_community.document_loaders import WebBaseLoader
@@ -19,13 +20,31 @@ from langchain_community.embeddings import OllamaEmbeddings
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import MessagesPlaceholder
 llm = Ollama(model="llama3", temperature=0)
-prompt = ChatPromptTemplate.from_messages([
-    ("user", "{input}"),
-    ("user", "Given the above English text, translate them into Simplified Chinese please. NOTE: This sentence is NOT included.")
-])
+# prompt = ChatPromptTemplate.from_messages([
+#     ("user", "{input}"),
+#     ("user", "Given the above English text, translate them into Simplified Chinese please. NOTE: This sentence is NOT included.")
+# ])
+prompt = ChatPromptTemplate.from_template("""Translate the text the provided English context into Simplified Chinese:
+
+<context>
+{input}
+</context>
+""")
+prompt = PromptTemplate.from_template("""Please translate some text. If the original text is in English, translate info Simplified Chinese, otherwise, translate into English. The output should include:
+1. The original text and its language.
+2. The target text and its language.
+If possible,  some cultural or contextual adjustments can be applied, to make it more idiomatic.
+
+ORIGINAL TEXT:
+{input}
+""")
 
 from langchain_core.output_parsers import StrOutputParser
 output_parser = StrOutputParser()
 trivial_chain = prompt | llm | output_parser
-ans = trivial_chain.invoke({"input": "It is the punch line"})
-print(ans)
+ans = trivial_chain.invoke({"input": "There is no way (short of OCR) to extract text from these files."})
+print(ans + "\n---")
+ans = trivial_chain.invoke({"input": "What is your glorious purpose?"})
+print(ans + "\n---")
+ans = trivial_chain.invoke({"input": """请你给我讲一个笑话"""})
+print(ans + "\n---")
